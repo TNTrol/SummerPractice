3c3db0395e0f6c95417dc1b0638353805adc1fb6
TNTrol <sem.go@bk.ru>
2021-08-03 11:59:47 +0300

    last version

---
+++ utils.h
@@ -0,0 +1,32 @@
+
+
+#ifdef __cplusplus
+extern "C" {                 // Make sure we have C-declarations in C++ programs
+#endif
+
+#ifndef UTILS_H
+#define UTILS_H
+#define DEBUG
+#include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/x509.h>
+#include <openssl/x509_vfy.h>
+#include <unistd.h>
+
+int create_socket(char url_str[], BIO *out);
+int scan_server(char url_str[]);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
